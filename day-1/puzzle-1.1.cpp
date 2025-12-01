243fa466bb4826e88a3846b1ffae76db78942b2a
Kierznowski <123631898+Kierznowski@users.noreply.github.com>
2025-12-01 15:16:29 +0100

    Initial commit

---
+++ day-1/puzzle-1.1.cpp
@@ -0,0 +1,74 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int count_zeros(std::vector<std::string>& moves);
+int count_new_method(std::vector<std::string>& moves);
+
+int main() {
+
+	//std::fstream file("test-input.txt");
+	std::fstream file("input.txt");
+
+	if (!file) {
+		std::cerr << "File cannot be opened!" << std::endl;
+		return 1;
+	}
+	
+	std::vector<std::string> moves;
+
+	std::string line;
+	while(getline(file, line)) {
+		moves.push_back(line);
+	}
+	file.close();
+
+	int password = count_zeros(moves);
+	int new_password = count_new_method(moves);
+
+	std::cout << "The password is: " << password << '\n';
+	std::cout << "The new password is: " << new_password << '\n';
+	return 0;
+}
+
+int count_zeros(std::vector<std::string>& moves) {
+	int pos = 50;
+	int count {0};
+
+	for (std::string move : moves) {
+		char dir = move[0];
+		if (dir == 'L') {
+			pos -= stoi(move.substr(1));
+		} else {
+			pos += stoi(move.substr(1));
+		}
+		pos %= 100;
+		if (pos == 0) count++;
+	}
+
+	return count;
+}
+
+int count_new_method(std::vector<std::string>& moves) {
+	int pos = 50;
+	int count = 0;
+
+	for (std::string move : moves) {
+		char dir = move[0];
+		int distance = stoi(move.substr(1));
+
+		for (int i = 0; i < distance; i++) {
+			if (dir == 'L') {
+				pos--;
+				if (pos < 0) pos = 99;
+			} else {
+				pos++;
+				if (pos > 99) pos = 0;
+			}
+
+			if (pos == 0) count++;
+		}
+	}
+	return count;
+}
+// 6561
